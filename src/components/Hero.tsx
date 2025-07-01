9911aeb5eab982ee9988e8ac6b28ae4d35d00346
wolfieelite <martetaylor333@gmail.com>
2025-06-30 20:28:53 -0700

    Impletmnets navigation section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Hero() {
+    return (
+        <section className='w-full nav-height bg-black relative'>
+            <div className="h-5/6"></div>
+        </section>
+    );
+}
+
+export default Hero;
